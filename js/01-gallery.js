ee349bc91853b85cb1ce37dac53c6ef9ee91d8c7
Oleksandr Chyhryn <as.chigrin@gmail.com>
2022-08-28 00:18:37 +0300

    Додає реалізацію двох завдань

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,50 @@
-import { galleryItems } from './gallery-items.js';
+import { galleryItems } from "./gallery-items.js";
 // Change code below this line
 
-console.log(galleryItems);
+const gallery = document.querySelector(".gallery");
+
+const galleryMarkup = galleryItems
+  .map(
+    (item) => `<div class="gallery__item">
+  <a class="gallery__link" href="${item.original}">
+    <img
+      class="gallery__image"
+      src="${item.preview}"
+      data-source="${item.original}"
+      alt="${item.description}"
+    />
+  </a>
+</div>`
+  )
+  .join("");
+
+gallery.insertAdjacentHTML("beforeend", galleryMarkup);
+
+gallery.addEventListener("click", modal);
+
+function modal(event) {
+  event.preventDefault();
+  const largeImg = event.target.dataset.source;
+
+  const instance = basicLightbox.create(
+    `
+      <img src="${largeImg}" width="800" height="600">
+  `,
+    {
+      onShow: (instance) => {
+        instance.element().querySelector("img").onclick = instance.close;
+
+        window.addEventListener("keydown", onPressEsc);
+      },
+    }
+  );
+
+  instance.show();
+
+  function onPressEsc(event) {
+    if (event.code === "Escape") {
+      instance.close();
+      window.removeEventListener("keydown", onPressEsc);
+    }
+  }
+}
